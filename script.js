25f15f46468ccd8ff193b17a2e844610795239cb
An-Sofie Olislaegers <ansofie.olislaegers@student.ucll.be>
2025-04-02 12:43:23 +0200

    eerste en tweede taak toevoegen

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 /* H1 aanpassen */
 document.querySelector('h1').innerText = 'Mijn To-Do Lijst';
 
+/*eerste taak toevoegen*/
+const taakTitel1 = 'Boodschappen doen';
+const taakElement1= document.createElement('article');
+taakElement1.innerHTML = `<h2>${taakTitel1}</h2>`;
+taakElement1.classList.add('taak');
+document.querySelector('section.Taken').appendChild(taakElement1);
+
+/*Tweede taak toevoegen*/
+const taakTitel2 = 'Sporten';
+const taakElement2 = document.createElement ('article');
+taakElement2.innerHTML = `<h2>${taakTitel2}</h2>`;
+document.querySelector('section.Taken').appendChild(taakElement2);
+
+
+/*verwijderen van 'geenTaken*/
+document.getElementById('geenTaken').remove();
+
 /* Taak toevoegen */
 function voegTaakToe(taak) {
     const taakElement = document.createElement('article');
     taakElement.innerHTML = `<h2>${taak.titel}</h2><p>${taak.beschrijving}</p>`;
     taakElement.classList.add('taak');
     document.querySelector ('section.Taken').appendChild(taakElement);
-    
+
 }
+
+
+
